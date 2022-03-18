5be8c104962bbc682714e6549300f3c6022cef84
Maxim Kupriianov <max@injectivelabs.org>
2022-03-18 04:28:37 +0100

    Added CustomReport closure to enable external custom reports.

--- reports.go
+++ reports.go
@@ -1,6 +1,6 @@
 package metrics
 
-func SlowSubscriberEventsDropped(amount int, tags ...Tags) {
+func CustomReport(reportFn func(s Statter, tagSpec []string), tags ...Tags) {
 	clientMux.RLock()
 	defer clientMux.RUnlock()
 
@@ -8,46 +8,29 @@ func SlowSubscriberEventsDropped(amount int, tags ...Tags) {
 		return
 	}
 
-	tagSpec := JoinTags(tags...)
+	reportFn(client, JoinTags(tags...))
+}
 
-	client.Count("pubsub.slow_subscriber.events_dropped", int64(amount), tagSpec, 1)
+func SlowSubscriberEventsDropped(amount int, tags ...Tags) {
+	CustomReport(func(s Statter, tagSpec []string) {
+		s.Count("pubsub.slow_subscriber.events_dropped", int64(amount), tagSpec, 1)
+	})
 }
 
 func SpotTradesBatchSubmitted(size int, tags ...Tags) {
-	clientMux.RLock()
-	defer clientMux.RUnlock()
-
-	if client == nil {
-		return
-	}
-
-	tagSpec := JoinTags(tags...)
-
-	client.Count("events.spot_trades_batch.size", int64(size), tagSpec, 1)
+	CustomReport(func(s Statter, tagSpec []string) {
+		s.Count("events.spot_trades_batch.size", int64(size), tagSpec, 1)
+	})
 }
 
 func DerivativeTradesBatchSubmitted(size int, tags ...Tags) {
-	clientMux.RLock()
-	defer clientMux.RUnlock()
-
-	if client == nil {
-		return
-	}
-
-	tagSpec := JoinTags(tags...)
-
-	client.Count("events.derivative_trades_batch.size", int64(size), tagSpec, 1)
+	CustomReport(func(s Statter, tagSpec []string) {
+		s.Count("events.derivative_trades_batch.size", int64(size), tagSpec, 1)
+	})
 }
 
 func IndexPriceUpdatesBatchSubmitted(size int, tags ...Tags) {
-	clientMux.RLock()
-	defer clientMux.RUnlock()
-
-	if client == nil {
-		return
-	}
-
-	tagSpec := JoinTags(tags...)
-
-	client.Count("events.set_price_batch.size", int64(size), tagSpec, 1)
+	CustomReport(func(s Statter, tagSpec []string) {
+		s.Count("events.set_price_batch.size", int64(size), tagSpec, 1)
+	})
 }
